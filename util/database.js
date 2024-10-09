2733533d5f174f31f07f22c6c3bad96675d6ff98
Mohamed Elkhattam <116581987+MohamedElkhattam@users.noreply.github.com>
2024-10-09 03:02:56 +0300

    Update database.js

--- util/database.js
+++ util/database.js
@@ -1,4 +1,4 @@
 const {Sequelize} = require('sequelize');
-const sequelize = new Sequelize('node-complete' , 'root' , 'MohamedKhaled@4' ,{dialect:'mysql',host:'localhost'})
+const sequelize = new Sequelize('node-complete' , 'root' , <MyPassword> ,{dialect:'mysql',host:'localhost'})
 
 module.exports = sequelize;
